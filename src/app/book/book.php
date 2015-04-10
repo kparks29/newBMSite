2b90b9ad0a9c7b6a8781f336fda68e4a73fe352b
Kendrick Parks <kendrick@homdna.com>
2015-04-09 22:06:22 -0700

    form just needs styling and message written, all fields present

--- src/app/book/book.php
+++ src/app/book/book.php
@@ -1,6 +1,5 @@
 <?php
-  $body = $_POST["message"];
-  $to      = "ocburgermonster@gmail.com";
+  $to = "ocburgermonster@gmail.com";
   $subject = "Event Request from Site";
   $request = $_POST["event"];
   $name = $_POST["name"];
@@ -11,9 +10,14 @@
   $city = $_POST["city"];
   $zip = $_POST["zip"];
   $date = $_POST["date"];
-  $date = $_POST["start"];
-  $date = $_POST["end"];
-  $message = $subject . "\r\n" . $body . "\r\n";
+  $start = $_POST["start"];
+  $end = $_POST["end"];
+  $eventName = $_POST["eventName"];
+  $number = $_POST["number"];
+  $company = $_POST["company"];
+  $veggie = $_POST["veggie"];
+  $promo = $_POST["promo"];
+  $message = $subject . "\r\n";
   $header = "To: Burger Monster <ocburgermonster@gmail.com>\r\n";
   $header.= "From: " . $name . " <" . $email . ">\r\n"; 
   $header.= "MIME-Version: 1.0\r\n"; 
@@ -26,6 +30,6 @@
   //the from in the header must also be an email address on the server
   mail($to, $subject, $message, $header);
   //redirect page
-  header( "Location: http://www.burgermonster.net" );
+  header( "Location: http://www.burgermonster.net/success" );
   exit();
 ?>
