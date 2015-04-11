3836b3cf25ffaa06bbce94945faea5a1ccef16c3
Kendrick Parks <kendrick@homdna.com>
2015-04-10 21:32:03 -0700

    mailers working

--- src/book.php
+++ src/book.php
@@ -17,9 +17,20 @@
   $company = $_POST["company"];
   $veggie = $_POST["veggie"];
   $promo = $_POST["promo"];
-  $message = $subject . "\r\n";
-  $header = "To: Burger Monster <ocburgermonster@gmail.com>\r\n";
-  $header.= "From: " . $name . " <" . $email . ">\r\n"; 
+  $message = "Type of Request: " . $request . "\r\n" . 
+             "Contact Name: " . $name . "\r\n" .
+             "How'd did you hear about us?: " . $hear . "\r\n" .
+             "Email: " . $email . "\r\n" .
+             "Phone Number: " . $phone . "\r\n" .
+             "Event Address: " . $address . " " . $city . " " . $zip . "\r\n" .
+             "Event Date: " . $date . "\r\n" .
+             "Event Time: " . $start . " - " . $end . "\r\n" .
+             "Event Name: " . $eventName . "\r\n" .
+             "Number of Guests: " . $number . "\r\n" .
+             "Company Name: " . $company . "\r\n" .
+             "Veggie Option: " . $veggie . "\r\n" .
+             "Promo Code: " . $promo . "\r\n";
+  $header.= "From: Burger Monster <info@burgermonster.net>\r\n"; 
   $header.= "MIME-Version: 1.0\r\n"; 
   $header.= "Content-Type: text/plain; charset=utf-8\r\n"; 
   $header.= "X-Priority: 1\r\n"; 
