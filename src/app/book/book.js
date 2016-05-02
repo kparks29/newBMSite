2235f432ccdf3221f8a2602c7f3799fe106d2b60
samjwalston <samjwalston@gmail.com>
2016-05-02 11:15:15 -0700

    added booking js updates

--- src/app/book/book.js
+++ src/app/book/book.js
@@ -3,7 +3,42 @@
 
   angular
     .module('Book', [])
-    .controller('BookCtrl', BookCtrl);
+    .controller('BookCtrl', ['jQuery', BookCtrl]);
 
-  function BookCtrl() { }
+  function BookCtrl(jQuery) {
+    var date       = jQuery('#date');
+    var startTime  = jQuery('#time-start');
+    var endTime    = jQuery('#time-end');
+
+    date.datetimepicker({
+      format:             'MMM Do YYYY',
+      minDate:            new Date(),
+      useCurrent:         false,
+      ignoreReadonly:     true,
+      widgetPositioning:  { horizontal: 'right' }
+    });
+
+    startTime.datetimepicker({
+      format:             'LT',
+      disabledHours:      [0,1,2,3,4,5,6,7],
+      ignoreReadonly:     true,
+      widgetPositioning:  { horizontal: 'right' }
+    });
+
+    endTime.datetimepicker({
+      format:             'LT',
+      useCurrent:         false,
+      disabledHours:      [3,4,5,6,7,8,9],
+      ignoreReadonly:     true,
+      widgetPositioning:  { horizontal: 'right' }
+    });
+
+    startTime.on('dp.change', function(event) {
+      endTime.data('DateTimePicker').minDate(event.date);
+    });
+
+    endTime.on('dp.change', function(event) {
+      startTime.data('DateTimePicker').maxDate(event.date);
+    });
+  }
 })();
